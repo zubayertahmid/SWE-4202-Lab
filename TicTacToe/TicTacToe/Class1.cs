aa743258e18b8168ea2e1e5707f57bad754f90c0
zubayertahmid <95120572+zubayertahmid@users.noreply.github.com>
2022-03-21 20:41:54 +0600

    TicTacToe
    Currently under development

---
+++ TicTacToe/TicTacToe/Class1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Board
+
+    {
+        public Player player1 = new Player();
+        public Player player2 = new Player();
+
+        public int[,] position = new int[3,3];
+
+        public Board(Player player1, Player player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    position[i, j] = 0;
+                }
+            }
+        }
+
+        public Board()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    position[i, j] = 0;
+                }
+            }
+        }
+
+        public void reset()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    position[i, j] = 0;
+                }
+            }
+        }
+
+        public void setPlayers(Player player1, Player player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public bool makeMove(int i, int j, bool player)
+        {
+            if (player)
+            {
+                if(position[i, j] == 0)
+                {
+                    position[i, j] = 1;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(position[i, j] == 0)
+                {
+                    position[i, j] = 1;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool isDraw()
+        {
+            for(int i = 0;i < 3; i++)
+            {
+                for(int j = 0;j < 3; j++)
+                {
+                    if(position[(i,j)] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool checkWinCondition()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                if(position[i,0]==position[i,1] && position[i,1]==position[i,2] && position[i, 0] != 0)
+                {
+                    if (position[i, 0] == 1)
+                    {
+                        return this.player1.isWinner = true;
+                    }
+                    else
+                    {
+                        return this.player1.isWinner = true;
+                    }
+                    return true;
+                }
+                
+            }
+        }
+    }
+
+
+    public class Player
+    {
+        public int score;
+        public bool isWinner;
+
+        public Player()
+        {
+            this.score = 0;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player-1: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player-2:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start New Game";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(348, 67);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(65, 56);
+            this.one.TabIndex = 3;
+            this.one.UseVisualStyleBackColor = true;
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(490, 67);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(65, 56);
+            this.three.TabIndex = 4;
+            this.three.UseVisualStyleBackColor = true;
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(419, 67);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(65, 56);
+            this.two.TabIndex = 5;
+            this.two.UseVisualStyleBackColor = true;
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(348, 129);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(65, 56);
+            this.four.TabIndex = 6;
+            this.four.UseVisualStyleBackColor = true;
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(419, 129);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(65, 56);
+            this.five.TabIndex = 7;
+            this.five.UseVisualStyleBackColor = true;
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(490, 129);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(65, 56);
+            this.six.TabIndex = 8;
+            this.six.UseVisualStyleBackColor = true;
+            // 
+            // seven
+            // 
+            this.seven.Location = new System.Drawing.Point(348, 191);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(65, 56);
+            this.seven.TabIndex = 9;
+            this.seven.UseVisualStyleBackColor = true;
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(419, 191);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(65, 56);
+            this.eight.TabIndex = 10;
+            this.eight.UseVisualStyleBackColor = true;
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(490, 191);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(65, 56);
+            this.nine.TabIndex = 11;
+            this.nine.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+    }
+}
+
